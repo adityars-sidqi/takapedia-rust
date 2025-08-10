b45ef063c3f5d91914a1d12608f4046b133693e6
Aditya Rahman Sidqi <adityars.sidqi@gmail.com>
2025-08-10 22:55:38 +0700

    feat: base service takapedia with Rust

--- payment-service/src/main.rs
+++ payment-service/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{serve, Router};
+use axum::routing::get;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
+    // build our application with a route
+    let app = Router::new()
+        // `GET /` goes to `root`
+        .route("/", get(|| { async { "Hello, World!" } }));
+
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    serve(listener, app).await.unwrap();
 }
